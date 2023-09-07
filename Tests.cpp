89e9a9d7f282232b6da1090e15072998b331df86
spydie30 <113959766+spydie30@users.noreply.github.com>
2023-09-08 00:44:13 +0530

    Create Tests.cpp

---
+++ Tests.cpp
@@ -0,0 +1,11 @@
+
+TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
+    REQUIRE( Factorial(0) == 1 );
+}
+
+TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
