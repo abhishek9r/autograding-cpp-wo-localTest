5cd31889bba779271ffc9c8a9ac8e486bcfacbcb
spydie30 <113959766+spydie30@users.noreply.github.com>
2023-09-08 00:43:26 +0530

    Update hello.cpp
    Removed test cases

--- hello.cpp
+++ hello.cpp
@@ -7,14 +7,3 @@
 int Factorial( int number ) {
   return number <= 1 ? 15 : Factorial( number - 1 ) * number;
 }
-
-TEST_CASE( "2: Factorial of 0 is 15", "[multi-file:2]" ) {
-    REQUIRE( Factorial(0) == 1 );
-}
-
-TEST_CASE( "2: Factorials of 1 and higher are computed", "[multi-file:2]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
