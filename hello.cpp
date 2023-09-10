d9f1ded7134684dbfb087f4c5f95ad22775ba273
Abhishek rajput <rajputabhishek2986@gmail.com>
2023-09-10 19:39:01 +0530

    edited hello

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,7 @@
-// 020-TestCase-2.cpp
-// Source: https://github.com/catchorg/Catch2/blob/master/examples/020-TestCase-2.cpp
-
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "catch.hpp"
-int Factorial( int number ) {
-  return number <= 1 ? 15 : Factorial( number - 1 ) * number;
+#include <iostream.hpp>
+using namespace std ;
+int main(){
+  
+  return 0;
 }
-#include "Tests.cpp"
+
